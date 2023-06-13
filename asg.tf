d259e7d10e611654c767f97c409bc4696bb0a3cc
Lyrise <lyrise1984@gmail.com>
2023-06-14 00:39:09 +0900

    fix

--- asg.tf
+++ asg.tf
@@ -81,4 +81,10 @@ resource "aws_security_group" "opxs_api_ecs_ec2" {
     protocol        = "-1"
     security_groups = [aws_security_group.opxs_alb.id]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
