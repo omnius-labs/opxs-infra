86a0713360624099113dfdcdc1d840f1d2c4fe67
Lyrise <lyrise1984@gmail.com>
2023-10-10 17:34:52 +0900

    fix

--- resources/ecr.tf
+++ resources/ecr.tf
@@ -1,5 +1,13 @@
 resource "aws_ecr_repository" "opxs_api" {
-  name = "opxs-api-ecr"
+  name = "opxs-api-ecs-ecr"
+}
+
+resource "aws_ecr_repository" "opxs_batch_email_send" {
+  name = "opxs-batch-email-send-lambda-ecr"
+}
+
+resource "aws_ecr_repository" "opxs_batch_email_send_feedback" {
+  name = "opxs-batch-email-send-feedback-lambda-ecr"
 }
 
 locals {
@@ -23,5 +31,15 @@ locals {
 
 resource "aws_ecr_lifecycle_policy" "opxs_api" {
   repository = aws_ecr_repository.opxs_api.name
+  policy     = jsonencode(local.ecr-lifecycle-policy)
+}
+
+resource "aws_ecr_lifecycle_policy" "opxs_batch_email_send" {
+  repository = aws_ecr_repository.opxs_batch_email_send.name
+  policy     = jsonencode(local.ecr-lifecycle-policy)
+}
+
+resource "aws_ecr_lifecycle_policy" "opxs_batch_email_send_feedback" {
+  repository = aws_ecr_repository.opxs_batch_email_send_feedback.name
   policy     = jsonencode(local.ecr-lifecycle-policy)
 }
