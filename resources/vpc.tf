6b2d50619ac5180a3c7d5befc0b3af6ab12cbd5a
Lyrise <lyrise1984@gmail.com>
2024-01-24 23:59:17 +0900

    fix

--- resources/vpc.tf
+++ resources/vpc.tf
@@ -18,25 +18,3 @@ module "nat" {
   private_subnets_cidr_blocks = module.vpc.private_subnets_cidr_blocks
   private_route_table_ids     = module.vpc.private_route_table_ids
 }
-
-resource "aws_internet_gateway" "opxs" {
-  vpc_id = module.vpc.vpc_id
-}
-
-resource "aws_route_table" "opxs" {
-  vpc_id = module.vpc.vpc_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.opxs.id
-  }
-}
-
-resource "aws_route_table_association" "opxs_public_1" {
-  route_table_id = aws_route_table.opxs.id
-  subnet_id      = module.vpc.public_subnets[0]
-}
-
-resource "aws_route_table_association" "opxs_public_2" {
-  route_table_id = aws_route_table.opxs.id
-  subnet_id      = module.vpc.public_subnets[1]
-}
