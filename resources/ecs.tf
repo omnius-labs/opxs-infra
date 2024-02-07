c9e1870307cf5cd16ce70eb7477f8e8b17f1cada
Lyrise <lyrise1984@gmail.com>
2024-02-07 19:13:32 +0900

    fix

--- resources/ecs.tf
+++ resources/ecs.tf
@@ -89,6 +89,122 @@ EOF
   task_role_arn            = aws_iam_role.opxs_api_ecs_task.arn
 }
 
+resource "aws_iam_role" "opxs_ecs_instance" {
+  name               = "opxs-ecs-instance-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "opxs_ecs_instance" {
+  role       = aws_iam_role.opxs_ecs_instance.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "opxs_ecs_instance" {
+  role = aws_iam_role.opxs_ecs_instance.name
+}
+
+resource "aws_iam_role" "opxs_api_ecs_tasks_execution" {
+  name               = "opxs-api-ecs-tasks-execution-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "opxs_api_ecs_tasks_execution" {
+  role       = aws_iam_role.opxs_api_ecs_tasks_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "opxs_api_ecs_task" {
+  name               = "opxs-api-ecs-task-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "opxs_api_ecs_task" {
+  role       = aws_iam_role.opxs_api_ecs_task.name
+  policy_arn = aws_iam_policy.opxs_api_ecs_task.arn
+}
+
+resource "aws_iam_policy" "opxs_api_ecs_task" {
+  name   = "opxs-api-ecs-tasks-policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue"
+      ],
+      "Resource": "${aws_secretsmanager_secret.opxs_api.id}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": "sqs:SendMessage",
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject"
+      ],
+      "Resource": "arn:aws:s3:::opxs.v1.dev.image-convert/*"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_cloudwatch_log_group" "opxs_api" {
   name              = "/aws/opxs-api-task/task-group"
   retention_in_days = 3
