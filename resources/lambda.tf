293c16a850cfef2e23d27251f346bf90c3c5f5e3
Lyrise <lyrise1984@gmail.com>
2024-12-11 20:25:58 +0900

    fix

--- resources/lambda.tf
+++ resources/lambda.tf
@@ -59,6 +59,16 @@ resource "aws_iam_policy" "opxs_api_lambda" {
 {
 	"Version": "2012-10-17",
 	"Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::opxs.v1.${var.run_mode}.file-convert/*"
+            ]
+        },
 		{
 			"Action": [
 				"secretsmanager:GetSecretValue"
